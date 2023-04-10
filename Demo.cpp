92c935fac1d07b5387a766d98f29c1bae03512d3
nirmeir <nirmeir11@gmail.com>
2023-04-10 21:37:22 +0300

    fix bugs

--- Demo.cpp
+++ Demo.cpp
@@ -18,18 +18,18 @@ using namespace std;
 #include "sources/card.hpp"
 using namespace ariel;
 
-int main() {
-  Player p1("Alice");
-  Player p2("Bob");
-  Game game(p1,p2);
-  cout<<"stack size is"<<p1.stacksize()<<endl;
-  cout<<"stack size is"<<p2.stacksize()<<endl;
-  
-  game.playAll();
+int main()
+{
+  // Player p1("Alice");
+  // Player p2("Bob");
+  // Game game(p1,p2);
+  // cout<<"stack size is"<<p1.stacksize()<<endl;
+  // cout<<"stack size is"<<p2.stacksize()<<endl;
 
+  // game.playAll();
 
-  int sum= p1.cardesTaken()+p2.cardesTaken()+p1.stacksize()+p2.stacksize();
-  cout<<"sum is"<<sum<<endl;
+  // int sum= p1.cardesTaken()+p2.cardesTaken()+p1.stacksize()+p2.stacksize();
+  // cout<<"sum is"<<sum<<endl;
   // cout<<"p1 size is"<<p1.Start_stack.size()<<endl;
   // cout<<"p2 size is"<<p2.Start_stack.size()<<endl;
   // cout<<"p1 card taken is"<<p1.cardesTaken()<<endl;
@@ -40,26 +40,25 @@ int main() {
   // game.printWiner();
   // game.printStats();
 
+  //     return 0;
+  // }
 
+  // Create two players with their names
+  Player p1("Alice");
+  Player p2("Bob");
 
-    
-    return 0;
+  Game game(p1, p2);
+  for (int i = 0; i < 5; i++)
+  {
+    game.playTurn();
+  }
+  game.printLastTurn();             // print the last turn stats. For example:
+                                    // Alice played Queen of Hearts Bob played 5 of Spades. Alice wins.
+                                    // Alice played 6 of Hearts Bob played 6 of Spades. Draw. Alice played 10 of Clubs Bob played 10 of Diamonds. draw. Alice played Jack of Clubs Bob played King of Diamonds. Bob wins.
+  cout << p1.stacksize() << endl;   // prints the amount of cards left. should be 21 but can be less if a draw was played
+  cout << p2.cardesTaken() << endl; // prints the amount of cards this player has won.
+  game.playAll();                   // playes the game untill the end
+  game.printWiner();                // prints the name of the winning player
+  game.printLog();                  // prints all the turns played one line per turn (same format as game.printLastTurn())
+  game.printStats();                // for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
 }
-  // Create two players with their names 
-  //  Player p1("Alice");
-  //  Player p2("Bob");
-
-  //  Game game(p1,p2); 
-  //  for (int i=0;i<5;i++) {
-  //   game.playTurn();
-  //  }
-  //  game.printLastTurn(); // print the last turn stats. For example:
-  //                                                   // Alice played Queen of Hearts Bob played 5 of Spades. Alice wins.
-  //                                                   // Alice played 6 of Hearts Bob played 6 of Spades. Draw. Alice played 10 of Clubs Bob played 10 of Diamonds. draw. Alice played Jack of Clubs Bob played King of Diamonds. Bob wins.
-  //  cout << p1.stacksize() << endl; //prints the amount of cards left. should be 21 but can be less if a draw was played
-  //  cout << p2.cardesTaken() << endl; // prints the amount of cards this player has won. 
-  //  game.playAll(); //playes the game untill the end
-  //  game.printWiner(); // prints the name of the winning player
-  //  game.printLog(); // prints all the turns played one line per turn (same format as game.printLastTurn())
-  //  game.printStats();// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
-
--- sources/card.cpp
+++ sources/card.cpp
@@ -8,58 +8,5 @@ using namespace std;
 namespace ariel
 {    
 
-    //  vector<Card> Card::Create_cards()
-    // {
-    //     std::vector<Card> deck;
-    //     for (int suit = Card::HEARTS; suit <= Card::SPADES; suit++)
-    //     {
-    //         for (int value = 1; value <= 13; value++)
-    //         {
-    //             deck.push_back(Card(value, (Card::Suit)suit));
-    //         }
-    //     }
-    //     return deck;
-    // }
-
-    // Shuffle the deck
-    // static vector<Card> Card::Shuffle()
-    // {
-    //     vector<Card> deck = Create_cards();
-
-    //     for (size_t i = deck.size() - 1; i > 0; i--)
-    //     {
-    //         unsigned int j = (size_t)std::rand() % deck.size();
-    //         // int j = std::rand() % (i + 1);
-    //         Card temp = deck[j];
-    //         deck[i] = deck[j];
-    //         deck[j] = temp;
-    //     }
-    //     return deck;
-    // }
 }
 
-// while (!deck.empty())
-// {
-//     v_deck.push_back(deck.top());
-//     deck.pop();
-// }
-// int random_num = std::rand() % v_deck.size();
-// std::shuffle(v_deck.begin(), v_deck.end(), random_num);
-// for (const auto &card : v_deck)
-// {
-//     deck.push(card);
-// }
-
-// std::cout << "Deck size after shuffle: " << deck.size() << std::endl;
-
-// int main()
-// {
-//     vector<Card> deck = Shuffle();
-//     while (!deck.empty())
-//     {
-//         Card card = deck[52];
-//         std::cout << card.getValue() << " of " << card.getSuitName() << std::endl;
-//         deck.pop_back();
-//     }
-//     return 0;
-// }
